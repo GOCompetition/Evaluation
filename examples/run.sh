3e4bd0fe61f1bf0c1e4e7fb3191bfa257a62cd93
Jesse Holzer <jesse.holzer@pnnl.gov>
2018-08-10 11:37:03 -0700

    add run.sh

---
+++ examples/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+case_dir='./case2/'
+raw=$case_dir'case.raw'
+rop=$case_dir'case.rop'
+con=$case_dir'case.con'
+inl=$case_dir'case.inl'
+sol1=$case_dir'sol1.txt'
+sol2=$case_dir'sol2.txt'
+#summary=$case_dir'summary.csv'
+#detail=$case_dir'detail.csv'
+
+# run it
+#python ../run.py "$raw" "$rop" "$con" "$inl" "$sol1" "$sol2" "$summary" "$detail"
+python ../run.py "$raw" "$rop" "$con" "$inl" "$sol1" "$sol2"
