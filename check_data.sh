2ae30a2ce0f3785ec9f79b6311efe10a343428b9
Jesse Holzer <jesse.holzer@pnnl.gov>
2019-03-16 18:14:22 -0700

    remove extra datasets

--- check_data.sh
+++ check_data.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 
-#case_dir='./examples/case2/'
-#raw=$case_dir'case.raw'
-#rop=$case_dir'case.rop'
-#con=$case_dir'case.con'
-#inl=$case_dir'case.inl'
-
-# uw case
-case_dir='./examples/test11/'
+case_dir='./examples/case2/'
 raw=$case_dir'case.raw'
-#rop=$case_dir'case.rop'
-rop=$case_dir'casev2.rop'
+rop=$case_dir'case.rop'
 con=$case_dir'case.con'
 inl=$case_dir'case.inl'
 
